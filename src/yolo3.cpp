d3e6dae6019e3ed3f797a8d47e5813eca506b1a6
justinbellucci <justin.bellucci@me.com>
2021-03-13 14:48:34 -0700

    small change

--- src/yolo3.cpp
+++ src/yolo3.cpp
@@ -1,4 +1,5 @@
 #include "yolo3.h"
+#include <memory>
 
 Yolo3::Yolo3(){}
 
@@ -12,35 +13,35 @@ void Yolo3::run(cv::String model_path, cv::String config_path)
 
 
 
-int Yolo3::testCamera()
-{
-    cv::VideoCapture capture(0);
+// int Yolo3::testCamera()
+// {
+//     cv::VideoCapture capture(0);
 
-    if(!capture.isOpened())
-    {
-        std::cout << "Error opening video stream!" << std::endl;
-        return -1;
-    }
+//     if(!capture.isOpened())
+//     {
+//         std::cout << "Error opening video stream!" << std::endl;
+//         return -1;
+//     }
 
-    while(1)
-    {
-        cv::Mat frame;
+//     while(1)
+//     {
+//         cv::Mat frame;
 
-        capture >> frame;
-        flip(frame, frame, 1);
+//         capture >> frame;
+//         flip(frame, frame, 1);
 
-        if(frame.empty())
-        {
-            break;
-        }
-        cv::imshow("Test frame", frame);
+//         if(frame.empty())
+//         {
+//             break;
+//         }
+//         cv::imshow("Test frame", frame);
 
-        char c = (char)cv::waitKey(1); 
-        if(c==27)
-        {
-            break;
-        }
-    }
-    capture.release();
-    return 0;
-}
+//         char c = (char)cv::waitKey(1); 
+//         if(c==27)
+//         {
+//             break;
+//         }
+//     }
+//     capture.release();
+//     return 0;
+// }
