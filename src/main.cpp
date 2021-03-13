cb231a9f79e6d80db68a0e5ad2b5d58ee39560eb
justinbellucci <justin.bellucci@me.com>
2021-03-13 14:47:28 -0700

    update model path

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 #include <iostream>
 
+cv::String model_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/yolov3.weights";
+cv::String config_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/yolov3.cfg";
+
 int main()
 {
     // do some testing here
     Yolo3 yolo3;
-    yolo3.testCamera();
+    yolo3.run(model_path, config_path);
+    //yolo3.testCamera();
 }
