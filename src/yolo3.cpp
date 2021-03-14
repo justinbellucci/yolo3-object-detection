3350c272f98d184a5a96372cc0ee4923a2f9abc7
justinbellucci <justin.bellucci@me.com>
2021-03-14 13:07:30 -0600

    add FrameQueue class to unique pointer

--- src/yolo3.cpp
+++ src/yolo3.cpp
@@ -1,47 +1,58 @@
 #include "yolo3.h"
 #include <memory>
 
-Yolo3::Yolo3(){}
+// constructor
+Yolo3::Yolo3(){
+    _capture = std::make_unique<cv::VideoCapture>();
+    _frames = std::make_unique<FrameQueue<cv::Mat>>();
+}
 
-Yolo3::~Yolo3(){}
+// destructor
+// set up thread barrier before object is destroyed
+Yolo3::~Yolo3(){
+    std::for_each(threads.begin(), threads.end(), [](std::thread &t) {
+        t.join();
+    });
+}
 
+// --- class methods ---
 void Yolo3::run(cv::String model_path, cv::String config_path)
 {
     Model model = Model::initialize(cv::String(model_path), cv::String(config_path));
     _model = std::make_unique<Model>(std::move(model));
+
+    // TODO: Add functionality for other video types
+    // open VideoCapture object with webcam (0)
+    _capture->open(0);
+    if(!_capture->isOpened())
+    {
+        std::cout << "Error opening video!" << std::endl;
+    }
+    // start frames capturing
+    startCaptureFramesThread();
+    CaptureFrames();
 }
 
+// start a new thread for frame capturing
+void Yolo3::startCaptureFramesThread()
+{
+    threads.emplace_back(std::thread(&Yolo3::CaptureFrames, this));
+}
 
+void Yolo3::CaptureFrames()
+{
+    cv::Mat frame;
+    while(true)
+    {
+        *_capture >> frame;
 
-// int Yolo3::testCamera()
-// {
-//     cv::VideoCapture capture(0);
-
-//     if(!capture.isOpened())
-//     {
-//         std::cout << "Error opening video stream!" << std::endl;
-//         return -1;
-//     }
-
-//     while(1)
-//     {
-//         cv::Mat frame;
-
-//         capture >> frame;
-//         flip(frame, frame, 1);
-
-//         if(frame.empty())
-//         {
-//             break;
-//         }
-//         cv::imshow("Test frame", frame);
-
-//         char c = (char)cv::waitKey(1); 
-//         if(c==27)
-//         {
-//             break;
-//         }
-//     }
-//     capture.release();
-//     return 0;
-// }
+        if(!frame.empty())
+        {
+            _frames->pushFrame(frame.clone());
+        } else 
+        {
+            break;
+        }
+    
+    }
+}
