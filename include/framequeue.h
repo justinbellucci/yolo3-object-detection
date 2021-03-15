7f71c78cc817247a095aeda6f3aecaa3af4eaa4b
justinbellucci <justin.bellucci@me.com>
2021-03-15 10:11:06 -0600

    fix - move template functions to header file

--- include/framequeue.h
+++ include/framequeue.h
@@ -7,12 +7,20 @@
 #include <opencv2/core.hpp>
 
 template <class T>
-class FrameQueue : private std::queue<T>
+class FrameQueue 
 {
 public:
     // constructor / destructor
     FrameQueue();
     ~FrameQueue();
+    // // copy constructor
+    // FrameQueue(const FrameQueue &source) = delete;
+    // // copy assignment operator
+    // FrameQueue &operator=(const FrameQueue &source) = delete;
+    // // move constructor
+    // FrameQueue(FrameQueue &&source) = delete;
+    // // move assignment operator
+    // FrameQueue &operator=(FrameQueue &&source) = delete;
 
     // methods 
     void pushFrame(const T &item);
@@ -21,6 +29,37 @@ public:
 
 private:
     std::mutex _mutex;
+    std::queue<T> _queue;
 };
+
+// ---- Template class function definitions
+
+// constructor
+template <typename T>
+FrameQueue<T>::FrameQueue(){}
+
+// destructor
+template <typename T>
+FrameQueue<T>::~FrameQueue(){}
+
+template <typename T>
+void FrameQueue<T>::pushFrame(const T &item)
+{
+    // perform queue modification under the lock
+    std::lock_guard<std::mutex> lckg(_mutex);
+    _queue.push(item);
+}
+
+template <typename T>
+T FrameQueue<T>::getFrame()
+{
+    // perform queue modification under the lock
+    std::lock_guard<std::mutex> lckg(_mutex);
+    // first in first out
+    T item = std::move(_queue.front());
+    _queue.pop();
+
+    return item;
+}
 
 #endif
