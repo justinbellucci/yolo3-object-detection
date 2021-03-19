d409ff31c4e848e464c67686ca8afb38413fdd6c
justinbellucci <justin.bellucci@me.com>
2021-03-19 15:33:11 -0600

    add preprocessor pointer to all class constructors

--- include/model.h
+++ include/model.h
@@ -1,6 +1,9 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "yoloconfig.h"
+#include "preprocessor.h"
+
 #include <memory>
 
 #include <opencv2/core.hpp>
@@ -11,29 +14,30 @@ class Model
 public:
     // constructor 
     Model() = delete;
-    //Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold);
     // destructor
     ~Model();
     // copy constructor
     Model(const Model &source) = delete;
     // copy assignment operator
     Model &operator=(const Model &source) = delete;
-    // move constructor
-    Model(Model &&source) noexcept;
-    // move assignment operator
-    Model &operator=(Model &&source) noexcept;
+    // // move constructor
+    Model(Model &&source);
+    // // move assignment operator
+    Model &operator=(Model &&source);
 
     static Model initialize(const cv::String &model, const cv::String &config, float confThreshold = 0.5, float nmsThreshold = 0.4,
                             const cv::String &framework = "", cv::dnn::Backend backend = cv::dnn::DNN_BACKEND_DEFAULT,
                             cv::dnn::Target target = cv::dnn::DNN_TARGET_CPU);
 
+    void processFrames(cv::Mat &frame, struct YoloConfig::FrameProcessingData &data);
+    cv::AsyncArray forward();
+
 private:
     // constructor - class object protected from creation outside scope
-    Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold);
-    
+    Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold, Preprocessor &preproc);
+
     std::unique_ptr<cv::dnn::Net> _net;
-    // _preprocessor;
-    // _postprocessor;
+    std::unique_ptr<Preprocessor> _preProcessor;
 
     float _confThreshold; // Confidence threshold
     float _nmsThreshold; // Non-maximum suppression threshold
--- src/model.cpp
+++ src/model.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 
 // constructor
-Model::Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold)
+Model::Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold, Preprocessor &preProc)
 {
     _net = std::make_unique<cv::dnn::Net>(std::move(net));
-
     _confThreshold = confThreshold;
     _nmsThreshold = nmsThreshold;
+    _preProcessor = std::make_unique<Preprocessor>(std::move(preProc));
+    
     std::cout << "CREATING instance of Model at " << this << std::endl;
 }
 
@@ -18,32 +19,30 @@ Model::~Model()
     std::cout << "DELETING instance of Model at " << this << std::endl;
 }
 
-// copy constructor
-//Model::Model(const Model &source) {}
-
-// copy assignment operator
-//Model &Model::operator=(const Model &source) {}
-
 // move constructor
-Model::Model(Model &&source) noexcept
+Model::Model(Model &&source) 
 {
     std::cout << "MOVING (c'tor) instance " << &source << " to instance " << this << std::endl;
     _net = std::move(source._net);
     _confThreshold = source._confThreshold;
     _nmsThreshold = source._nmsThreshold;
+    _preProcessor = std::move(source._preProcessor);
 
     source._net = nullptr;
     source._confThreshold = 0.0;
     source._nmsThreshold = 0.0;
+    source._preProcessor = nullptr;
 }
 
 // move assignment operator
-Model &Model::operator=(Model &&source) noexcept
+Model &Model::operator=(Model &&source) 
 {
     std::cout << "MOVING (assign) instance " << &source << " to instance " << this << std::endl;
     _net = std::move(source._net);
+    _preProcessor = std::move(source._preProcessor);
 
     source._net = nullptr;
+    source._preProcessor = nullptr;
 
     return *this;
 }
@@ -54,6 +53,18 @@ Model Model::initialize(const cv::String &model, const cv::String &config, float
     cv::dnn::Net net = cv::dnn::readNet(model, config, framework);
     net.setPreferableBackend(backend);
     net.setPreferableTarget(target);
+    // instantiate preProcessor object of type Preprocessor
+    Preprocessor preProcessor;
 
-    return Model(net, confThreshold, nmsThreshold);
+    return Model(net, confThreshold, nmsThreshold, preProcessor);
+}
+
+void Model::processFrames(cv::Mat &frame, struct YoloConfig::FrameProcessingData &data)
+{
+    _preProcessor->process(frame, *_net, data);
+}
+
+cv::AsyncArray Model::forward()
+{
+    return _net->forwardAsync();
 }
