41a7a5b303a2d33a22819d7f42cbee00ddcf2205
justinbellucci <justin.bellucci@me.com>
2021-03-31 16:24:48 -0600

    remove preprocessor class implementation

--- include/model.h
+++ include/model.h
@@ -2,13 +2,13 @@
 #define MODEL_H
 
 #include "yoloconfig.h"
-#include "preprocessor.h"
 
 #include <memory>
+#include <iostream>
 
-#include <opencv2/core.hpp>
 #include <opencv2/dnn.hpp>
-#include <opencv2/core/async.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 
 class Model 
 {
@@ -27,20 +27,21 @@ public:
     Model &operator=(Model &&source) noexcept;
 
     static Model initialize(const cv::String &model, const cv::String &config, float confThreshold = 0.5, float nmsThreshold = 0.4,
-                            const cv::String &framework = "", cv::dnn::Backend backend = cv::dnn::DNN_BACKEND_OPENCV,
-                            cv::dnn::Target target = cv::dnn::DNN_TARGET_CPU);
+                            cv::dnn::Backend backend = cv::dnn::DNN_BACKEND_DEFAULT, cv::dnn::Target target = cv::dnn::DNN_TARGET_CPU);
 
     void processFrames(cv::Mat &frame, struct YoloConfig::FrameProcessingData &data);
-    void forward();
+    std::vector<cv::Mat> forward();
 
-    std::vector<cv::String> getOutputNames(cv::dnn::Net &net);
+    void postProcessFrames(cv::Mat &frame, const std::vector<cv::Mat> &outs, std::vector<std::string> &names);
+    void drawPreds(int classId, float conf, int left, int top, int right, int bottom, cv::Mat &frame, std::vector<std::string> &names);
+
+    std::vector<cv::String> getOutputNames();
     
 private:
     // constructor - class object protected from creation outside scope
-    Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold, Preprocessor &preproc);
+    Model(cv::dnn::Net &net, float confThreshold, float nmsThreshold);
 
     std::unique_ptr<cv::dnn::Net> _net;
-    std::unique_ptr<Preprocessor> _preProcessor;
 
     float _confThreshold; // Confidence threshold
     float _nmsThreshold; // Non-maximum suppression threshold
