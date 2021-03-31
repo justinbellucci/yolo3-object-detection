244db6681b1d90f74effcfe4eb4ebc9166fc98fd
justinbellucci <justin.bellucci@me.com>
2021-03-31 15:07:33 -0600

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,17 @@
 
 #include <iostream>
 
-cv::String model_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/yolov3.weights";
+cv::String weights_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/yolov3.weights";
 cv::String config_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/yolov3.cfg";
 cv::String classNames_path = "/Users/justinbellucci/GitHub/yolo3-object-detection/coco.names";
 
-int main()
+const char* keys =
+"{help h usage ? | | Usage examples: \n\t\t./yolo3_detector --video=run_sm.mp4}"
+"{video v       |<none>| input video   }"
+;
+
+int main(int argc, char** argv)
 {
     
-    // instantiate struct object to hold frame processing data
-    YoloConfig::FrameProcessingData data;
-    data.inpWidth = 416;
-    data.inpHeight = 416;
-
-    // run main process
-    Yolo3 yolo3(data);
-    yolo3.run(model_path, config_path, classNames_path);
 }
+
